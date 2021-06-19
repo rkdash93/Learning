cdbfd9139bcecdb1c1738a58de44f7ff285d4566
Ram <ramakrishnadash97@gmail.com>
2021-06-19 17:35:01 +0530

    added what is feature for wikipedia, start-stop music changes, webbrowser feature for google and youtube

--- python learning/Jarvis_v2/Jarvis_alt.py
+++ python learning/Jarvis_v2/Jarvis_alt.py
@@ -5,6 +5,7 @@ import random
 import os,signal
 import subprocess
 import psutil
+import webbrowser
 from pyttsx3.drivers import sapi5
 from playsound import playsound
 from datetime import datetime
@@ -136,34 +137,62 @@ if __name__ == "__main__":
                     print('Playing music....')
                     music_files = os.listdir('.\\audio')
                     audio = random.choice(music_files)
-                    #os.startfile(f'.\\audio\\{audio}')
-                    shell_process = subprocess.Popen([f'.\\audio\\{audio}'],shell=True)
-                    parent = psutil.Process(shell_process.pid)
-                    print(parent.pid)
+                    os.startfile(f'.\\audio\\{audio}')
+                    #shell_process = subprocess.Popen([f'.\\audio\\{audio}'],shell=True)
+                    #parent = psutil.Process(shell_process.pid)
+                    #parent = psutil.Process(shell_process.pid)
+                    #print(parent.pid)
                     #while(parent.children() == []):
                         #continue
-                    children = parent.children(recursive=True)                    
+                    #children = parent.children(recursive=True)                    
                     #children = parent.children(recursive=True)
-                    print(children)
+                    #print(children)
                     #child_pid = children[0].pid
                     #print(child_pid)
                     
                 elif 'stop music' in cmd:
                     speak('Stopping music....')
                     print('Stopping music....')
-                    #subprocess.check_output("Taskkill /PID %d /F" % shell_process.pid)
-                    os.system("taskkill  /F /pid "+str(shell_process.pid))                    
+                    #subprocess.check_output("Taskkill /PID %d /F" % child_pid)
+                    #os.system("taskkill  /F /pid "+str(shell_process.pid))
+                    os.system("taskkill /f /im  WWAHost.exe")
+                    #print(children)                   
                 elif 'tell me about' in cmd:
                     speak('Searching wikipedia')
                     print('Searching wikipedia....')
-                    cmd = cmd.replace('tell me about', '')
+                    cmd = cmd.replace('tell me about ', '')
                     try:
                         results = wikipedia.summary(cmd,sentences=2)
                         print(results)
                         speak(results)
                     except Exception as e:
                         print('No results found')
-                        speak('No results found')   
+                        speak('No results found')
+                elif 'what is' in cmd:
+                    speak('Searching wikipedia')
+                    print('Searching wikipedia....')
+                    cmd = cmd.replace('what is ', '')
+                    try:
+                        results = wikipedia.summary(cmd,sentences=2)
+                        print(results)
+                        speak(results)
+                    except Exception as e:
+                        print('No results found')
+                        speak('No results found')                        
+                elif 'youtube' in cmd:
+                    speak('Opening youtube')   
+                    print('Opening YouTube.....')
+                    webbrowser.open('www.youtube.com')
+                elif 'google' in cmd:
+                    speak('Opening google')   
+                    print('Opening google.....')
+                    webbrowser.open('www.google.com')
+                elif 'search' in cmd:
+                    cmd = cmd.replace('search ','')
+                    speak('Searching in google')   
+                    print('Searching in google.....')
+                    webbrowser.open('https://www.google.com/search?q=' + str(cmd) )                                        
+
                                         
                 elif 'bye' in cmd:
                     break                         
