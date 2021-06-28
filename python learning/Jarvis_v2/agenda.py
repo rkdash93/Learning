7a5bd5d491936053da75961a698d436a18f859ac
Ram <ramakrishnadash97@gmail.com>
2021-06-28 11:42:22 +0530

    Remind function was deleted. It has been added again

--- python learning/Jarvis_v2/agenda.py
+++ python learning/Jarvis_v2/agenda.py
@@ -32,3 +32,25 @@ def agenda_func(tsk):
         f.close()
 
     return(1)    
+
+def remind_func(tsk):
+    if tsk == 'remind':
+        print('What should I remind?')
+        speak('What should I remind?')
+        rem_task = task().lower()
+        print('What should be the time?')
+        speak('What should be the time?')
+        time_task = task().lower()
+        print('Remind you to ' + rem_task + ' at ' + time_task + '. Confirm?')
+        speak('Remind you to ' + rem_task + ' at ' + time_task + '. Confirm?')
+        conf_task = task().lower()
+        if 'yes' in conf_task or 'yash' in conf_task:
+            print('Reminder added')
+            speak('Reminder added')
+            time_task = datetime.strptime(time_task.replace('.',''),'%I:%M %p')
+            return rem_task,time_task
+        else:
+            print('No reminder added')
+            speak('No reminder added')
+            time_task = datetime.strptime('12:00 AM','%I:%M %p')
+            return rem_task,time_task
